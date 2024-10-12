0cba285a0434f23d217c35434c68ad6ebdd228d2
Aryan Damaraju <aryan.damaraju@gmail.com>
2024-10-12 15:10:08 -0400

    new branch push

---
+++ ecom_front/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Login from "./functions/Login";
+import Signup from "./functions/Signup";
+import Home from "./functions/Home";
+
+function App() {
+ return (
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path ="/login" element={<Login/>}/>
+            <Route path ="/signup" element={<Signup/>}/>
+            <Route path ="/home" element={<Home/>}/>
+          </Routes>
+        </Router>
+      </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+export default App;
