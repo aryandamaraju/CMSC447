31d9d207619d52eb5a0bddccfacf7627cb58f77a
Aniket-Camarushi <aniketcam@gmail.com>
2024-10-16 00:27:57 -0400

    Added logo & formatted

--- ecom_front/src/functions/Home.js
+++ ecom_front/src/functions/Home.js
@@ -5,12 +5,15 @@ import { Link } from 'react-router-dom';
 import cart from "./img/cart.png"
 import star from "./img/star.png"
 import loginIcon from "./img/login.png"
+import InstaHubLogo from "./img/Insta Hub Logo.png"
 
 function Home(){
     return(
         <div className="home-page">
             <div className="header">
-                <div className="logo">Insta Hub</div>
+                <Link to="/" className="logo">
+                    <button className="logo_button"><img style={{width:150, height:44}} src={InstaHubLogo} alt="Insta Hub"/> </button>
+                </Link>
                 <div className="search-container">
                     <input type="text" placeholder="Search" className="search-bar"/>
                 </div>
@@ -19,7 +22,7 @@ function Home(){
                     <button className="button"><img style={{width:26, height:26}} src={star} alt="Star"/> </button>
 
                     <Link to="/login">
-                        <button className="button"><img style={{width:68, height:26}} src={loginIcon} alt="Login"/></button>
+                        <button className="button"><img style={{width:58, height:26}} src={loginIcon} alt="Login"/></button>
                     </Link>
                 </div>
             </div>
