efe5644bdfacdc54a0f1b17c52fc246511c24780
AlanZhang2003 <ga71939@umbc.edu>
2024-10-16 19:22:09 -0400

    Added Header

--- ecom_front/src/App.js
+++ ecom_front/src/App.js
@@ -5,16 +5,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from "./functions/Login";
 import Signup from "./functions/Signup";
 import Home from "./functions/Home";
+import Cart from "./functions/Cart";
 
 function App() {
  return (
       <div>
         <Router>
           <Routes>
-            <Route path="/" element={<Login />} />
+            <Route path="/" element={<Home/>} />
             <Route path ="/login" element={<Login/>}/>
             <Route path ="/signup" element={<Signup/>}/>
             <Route path ="/home" element={<Home/>}/>
+            <Route path ="/cart" element={<Cart/>}/>
           </Routes>
         </Router>
       </div>
--- ecom_front/src/functions/Home.js
+++ ecom_front/src/functions/Home.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import './Home.css';
+import Header from "./Header";
+import ProductList from "./ProductList";
 import { Link } from 'react-router-dom';
-
-import cart from "./img/cart.png"
-import star from "./img/star.png"
-import loginIcon from "./img/login.png"
 
 function Home(){
     return(
         <div className="home-page">
-            <div className="header">
-                <div className="logo">Insta Hub</div>
-                <div className="search-container">
-                    <input type="text" placeholder="Search" className="search-bar"/>
-                </div>
-                <div className="header-buttons">
-                    <button className="button"><img style={{width:26, height:26}} src={cart} alt="Cart"/> </button>
-                    <button className="button"><img style={{width:26, height:26}} src={star} alt="Star"/> </button>
-
-                    <Link to="/login">
-                        <button className="button"><img style={{width:68, height:26}} src={loginIcon} alt="Login"/></button>
-                    </Link>
-                </div>
-            </div>
+            <Header />
+            
         </div>
     )
 }
